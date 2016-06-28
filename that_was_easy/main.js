b53ec605f20d0225fe98f7738b32a66d2d2c0619
Gina <ginabrin.gb@gmail.com>
2016-06-28 23:24:23 +0000

    create a 'that was easy' button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,14 +1,12 @@
 function sayThatWasEasy() {
-  var thatWasEasy = new Audio("that_was_easy.mp3");
+  var thatWasEasy = new Audio("avadakedavra.mp3");
   thatWasEasy.play();
 }
-
-$("#easy").on("click", sayThatWasEasy);
-
+$("#easy").on("click", sayThatWasEasy); 
 $(document).keypress(delegateKeypress);
-
 function delegateKeypress(event) {
-  if (event.keyCode == 32) {
-    $("#easy").trigger("click");
-  }
+    
+}
+if (event.keyCode == 32) {
+
 }
