90abc2f041c3478d0efd894c2c107b6b0aea51ec
Gina <ginabrin.gb@gmail.com>
2016-07-01 23:46:09 +0000

    create workout buddy

--- workout_buddy/main.js
+++ workout_buddy/main.js
@@ -1,6 +1,8 @@
+var map 
+
 function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
+    map = new google.maps.Map(mapDiv, {
         center: {
             lat: 44.540,
             lng: -78.546
@@ -9,31 +11,34 @@ function initMap() {
     });
 }
 
+function createMarker(lat,lng) {
+    var marker = new google.maps.Marker({
+        position: {
+            lat: lat,
+            lng: lng
+        },
+        map: map,
+    });
+}
+
 function geoFindMe() {
-  var output = document.getElementById("out");
+    var output = document.getElementById("out");
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    function success(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        createMarker(latitude, longitude)
+        map.setCenter({lat: latitude, lng: longitude})
+    };
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    };
 
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.getCurrentPosition(success, error);
 }
